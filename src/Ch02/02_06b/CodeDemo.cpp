b76a1805823bef0f943b63ba81fc78afdfc8b466
zbelvin <belvinze@gmail.com>
2024-06-08 20:26:47 +0000

    beep

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,8 +3,19 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
 
+#define CAPACITY 5000
+#define DEBUG
 int main(){
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+#ifdef DEBUG
+    std::cout << "[GONNA ADD NOW OK??]" << std::endl;
+#endif
+    large += small;
+    std::cout << "The large integer is " << large << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,8 +3,20 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 int main(){
+    const size_t LENGTH1 = 25;
+    char array_str1[LENGTH1] = "Hey guys! ";
+    char array_str2[] = "What's up?";
+
+    std::string std_str1 = "Hewwo evewybody! ";
+    std::string std_str2 = "How am yuo?";
+
+    strncat(array_str1, array_str2, LENGTH1);
+    std::cout << array_str1 << std::endl;
+    std::cout << std_str1 + std_str2 << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
