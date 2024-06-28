bf7c8a00bbbd5594a17ad082d6344a35dda16018
zbelvin <belvinze@gmail.com>
2024-06-28 20:40:06 +0000

    bbb

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -8,7 +8,22 @@
 int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
     
-    std::vector<int>::iterator ptr = numbers.begin();
+    // std::vector<int>::iterator ptr = numbers.begin(); fuck that. auto.
+    auto ptr = numbers.begin();
+
+    while(ptr != numbers.end()){
+        std::cout << *ptr << " ";
+        ptr = next(ptr, 1);
+    }
+
+    std::cout << std::endl;
+
+    int i = 0;
+    do{
+        std::cout << numbers[i] << " ";
+        i++;
+    } while (i < numbers.size());
+    std::cout << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
